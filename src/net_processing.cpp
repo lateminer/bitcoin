76b2b5481f1598eb0f358d366466def2bb2d55d3
lateminer <late-miner@ya.ru>
2019-01-14 23:03:34 +0300

    Bump protocol version to 70016

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3205,9 +3205,9 @@ bool SendMessages(CNode* pto, CConnman& connman, const std::atomic<bool>& interr
 
 bool ProcessNetBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock, bool fForceProcessing, bool* fNewBlock, CNode* pfrom, CConnman& connman)
 {
-    bool isLegacy = false;
-    if (pfrom && pfrom->nVersion < CANONICAL_BLOCK_SIG_VERIFY_NEW_VERSION)
-        isLegacy = true;
+    bool isLegacy = true;
+    if (pfrom && pfrom->nVersion >= CANONICAL_BLOCK_SIG_VERIFY_NEW_VERSION)
+        isLegacy = false;
 
     // Check if block signature is canonical
     if (!IsCanonicalBlockSignature(pblock, isLegacy))
--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,7 +21,7 @@ static const int GETHEADERS_VERSION = 31800;
 static const int CANONICAL_BLOCK_SIG_VERSION = 60018;
 
 //! use new block signatures verification starting from this version
-static const int CANONICAL_BLOCK_SIG_VERIFY_NEW_VERSION = 70015;
+static const int CANONICAL_BLOCK_SIG_VERIFY_NEW_VERSION = 70016;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = CANONICAL_BLOCK_SIG_VERSION;
