48fa76c9c77484485331d10401942b938ba3def3
lateminer <late-miner@ya.ru>
2019-01-27 18:58:23 +0300

    Growification: protocol versioning

--- src/version.h
+++ src/version.h
@@ -11,33 +11,36 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70914;
+static const int PROTOCOL_VERSION = 40003;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+static const int GETHEADERS_VERSION = 30000;
+
+//! demand canonical block signatures starting from this version
+static const int CANONICAL_BLOCK_SIG_VERSION = 40001;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70912;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70914;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = PROTOCOL_VERSION; // use PROTOCOL_VERSION for now
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = PROTOCOL_VERSION; // use PROTOCOL_VERSION for now
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70913;
+static const int MIN_PEER_MNANNOUNCE = 40003;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 30000;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 30000;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 30000;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
+static const int NO_BLOOM_VERSION = 40002;
 
 
 #endif // BITCOIN_VERSION_H
