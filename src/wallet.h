47a51a7845bf561f20a7bb57db2d4159594db5db
lateminer <late-miner@ya.ru>
2019-03-03 21:49:40 +0300

    PoS: set nStakeSplitThreshold to 5000

--- src/wallet.h
+++ src/wallet.h
@@ -312,7 +312,7 @@ public:
 
         // Stake Settings
         nHashDrift = 45;
-        nStakeSplitThreshold = 2000;
+        nStakeSplitThreshold = 5000;
         nHashInterval = 22;
         nStakeSetUpdateTime = 300; // 5 minutes
 
