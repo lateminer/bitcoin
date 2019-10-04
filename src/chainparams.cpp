7ba31d8589b67899b97d3d7d3ab445fd4d19a8ec
lateminer <9951982+lateminer@users.noreply.github.com>
2019-10-04 15:35:33 +0300

    Delay PoSV3 activation until unknown point in future

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -101,7 +101,7 @@ public:
         consensus.nDifficultyDigiShield = 280000; 
         consensus.nCoinbaseMaturitySwitch = 280000;
         consensus.nCheckPOWFromnTime = 1414964233;
-        consensus.nProtocolV3Time = 1444028400;
+        consensus.nProtocolV3Time = std::numeric_limits<int64_t>::max();
 
         consensus.nLastPOWBlock = 974999;
         consensus.nStakeTimestampMask = 0xf; // 15
@@ -203,7 +203,7 @@ public:
         consensus.nDifficultyDigiShield = 10; 
         consensus.nCoinbaseMaturitySwitch = -1;
         consensus.nCheckPOWFromnTime = 1414964233;
-        consensus.nProtocolV3Time = 1577836800;
+        consensus.nProtocolV3Time = std::numeric_limits<int64_t>::max();
 
         consensus.nLastPOWBlock = 1000;
         consensus.nStakeTimestampMask = 0xf;
@@ -352,7 +352,7 @@ public:
         consensus.nDifficultyDigiShield = 10; 
         consensus.nCoinbaseMaturitySwitch = -1;
         consensus.nCheckPOWFromnTime = 1414964233;
-        consensus.nProtocolV3Time = 1444028400;
+        consensus.nProtocolV3Time = std::numeric_limits<int64_t>::max();
 
         consensus.nLastPOWBlock = 1000;
         consensus.nStakeTimestampMask = 0xf;
