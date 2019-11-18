a1a415d48c61c98e7f772e7568f193ab951cfc8f
lateminer <9951982+lateminer@users.noreply.github.com>
2019-11-18 23:22:30 +0300

    Enable script checks

--- src/main.cpp
+++ src/main.cpp
@@ -2486,16 +2486,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         pindex->hashProofOfStake = hashProofOfStake;
     }
 
-    // Potcoin ToDo: enable script checks
-    // bool fScriptChecks = true;
-    bool fScriptChecks = chainparams.GetConsensus().IsProtocolV3(block.GetBlockTime());
-    if (fCheckpointsEnabled) {
-        CBlockIndex *pindexLastCheckpoint = Checkpoints::GetLastCheckpoint(chainparams.Checkpoints());
-        if (pindexLastCheckpoint && pindexLastCheckpoint->GetAncestor(pindex->nHeight) == pindex) {
-            // This block is an ancestor of a checkpoint: disable script checks
-            fScriptChecks = false;
-        }
-    }
+    bool fScriptChecks = true;
 
     int64_t nTime1 = GetTimeMicros(); nTimeCheck += nTime1 - nTimeStart;
     LogPrint("bench", "    - Sanity checks: %.2fms [%.2fs]\n", 0.001 * (nTime1 - nTimeStart), nTimeCheck * 0.000001);
