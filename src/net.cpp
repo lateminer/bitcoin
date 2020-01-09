49b0d573112a22665efc781d60f76fe815ad67ca
lateminer <9951982+lateminer@users.noreply.github.com>
2020-01-09 22:11:37 +0300

    Show 'Potcoin', not 'Bitcoin' in UPnP Port Mapping description

--- src/net.cpp
+++ src/net.cpp
@@ -1414,7 +1414,7 @@ void ThreadMapPort()
             }
         }
 
-        std::string strDesc = "Bitcoin " + FormatFullVersion();
+        std::string strDesc = "Potcoin " + FormatFullVersion();
 
         try {
             while (true) {
