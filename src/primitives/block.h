5d08a0bf2b1aacae0d4c4033bf29c155123bd87a
lateminer <9951982+lateminer@users.noreply.github.com>
2022-01-19 20:16:56 +0200

    Fix 'warning: assigning field to itself'

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -41,7 +41,6 @@ public:
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
         READWRITE(this->nVersion);
-        nVersion = this->nVersion;
         READWRITE(hashPrevBlock);
         READWRITE(hashMerkleRoot);
         READWRITE(nTime);
