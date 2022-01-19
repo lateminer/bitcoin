c17a6fd56033647e630d4a74952e0fa653931c51
lateminer <9951982+lateminer@users.noreply.github.com>
2022-01-19 17:37:30 +0200

    Increase fee in PoS 3.1 hard fork

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -221,10 +221,9 @@ CAmount GetMinFee(size_t nBytes, uint32_t nTime)
 {
     CAmount nMinFee;
 
-    // Blackcoin ToDo: 
-    //if (IsProtocolV3_1(nTime))
-    //    nMinFee = (1 + (CAmount)nBytes / 1000) * MIN_TX_FEE;
-    //else
+    if (IsProtocolV3_1(nTime))
+        nMinFee = (1 + (CAmount)nBytes / 1000) * MIN_TX_FEE;
+    else
         nMinFee = MIN_TX_FEE;
 
     if (!MoneyRange(nMinFee))
